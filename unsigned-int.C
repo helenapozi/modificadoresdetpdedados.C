af150ccfa050e038cc714fddb4edc41be38435a2
helenapozi <helenapozi28@gmail.com>
2025-02-17 14:47:16 +0000

    unsigned

--- unsigned-int.C
+++ unsigned-int.C
@@ -1,1 +1,14 @@
 
+#include<stdio.h>
+
+int main (){
+int numerosinal = 3000000000;     //este valor excede valor o valor de um int comum;
+unsigned int numerosemsinal = 30000000;
+
+printf ("numerocomsinal:%d\n", numerosinal);
+printf ("numerosemsinal:%d\n", numerosemsinal);
+
+return 0;
+
+
+}
