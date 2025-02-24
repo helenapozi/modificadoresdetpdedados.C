47c14964688a41bfbd03fedbccfd22b11cdf0648
helenapozi <helenapozi28@gmail.com>
2025-02-24 13:42:15 +0000

    double

--- double.C
+++ double.C
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main (){
+double numeropreciso =3.141592653589793;
+long double numeromuitopreciso = 3.141592653589793236;
+
+printf("numeropreciso(double):%.15f\n", numeropreciso);
+printf ("numeromuitopreciso(longdouble):%.21lf\n", numeromuitopreciso);
+
+return 0;
+
+}
